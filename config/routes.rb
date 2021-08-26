3571d050c8dc4d49568d361e9b65120f7f92c276
SirawitN <nine_sirawit@hotmail.com>
2021-08-26 20:43:39 +0700

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   get 'main/abc'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get 'main/welcome', to: 'main#welcome'    #class#func_n
-  get 'main/contact_us', to: 'main#contact'
+  get '/welcome', to: 'main#welcome'    #class#func_n
+  get '/contact_us', to: 'main#contact'
 
 
 end
