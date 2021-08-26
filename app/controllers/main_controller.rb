5bfc83fb44493e1d2c906b50be27b47bc87d08ac
SirawitN <nine_sirawit@hotmail.com>
2021-08-26 16:26:23 +0700

    class2 works_01_welcome&contactPages

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,13 @@
+class MainController < ApplicationController
+  def abc
+  end
+
+  def welcome
+  end
+
+  def contact
+    @email = "6230556021@student.chula.ac.th"
+    @git = "https://github.com/SirawitN"
+  end
+
+end
